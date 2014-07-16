826719231eba06a0ef5c61b45601321a2e22a4c7
tmngassa <tmngassa@hotmail.com>
2014-07-15 23:08:27 -0700

    First commit

---
+++ src/main/java/com/tmngassa/gitdemo/App.java
@@ -0,0 +1,13 @@
+package com.tmngassa.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
